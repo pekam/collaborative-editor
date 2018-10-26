eb3d85a32c668884e8de41d9e33b2eecc1732d11
Pekka Maanpaa <pekkamaa@vaadin.com>
2018-10-26 11:05:30 +0300

    Initial commit
    based on skeleton-starter-flow

---
+++ README.md
@@ -0,0 +1,5 @@
+# Collaborative Editor
+
+WIP Prototyping collaborative editing with Vaadin.
+
+Run using `mvn jetty:run` and open [http://localhost:8080](http://localhost:8080).
---
+++ src/main/java/org/vaadin/pekkam/MainView.java
@@ -0,0 +1,13 @@
+package org.vaadin.pekkam;
+
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        add(new Label("Foo"));
+    }
+}
